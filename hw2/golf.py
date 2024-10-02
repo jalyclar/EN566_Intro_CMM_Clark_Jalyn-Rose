3bf465b4e0b35e9f8444a3b86ac76bac0f5438b6
jalyclar <jalynroseclark@gmail.com>
2024-10-02 14:49:04 -0400

    quick changes

--- hw2/golf.py
+++ hw2/golf.py
@@ -21,7 +21,9 @@ class golfball:
         self.rho=1.29*1000 #Density of air at sea level (g/m^3) 
         self.a=0.0014 #Frontal area of the golf ball (m^2)
         self.g=9.8 #Gravity (m/s^2)
-        self.delta_t=0.1 #time step (seconds) 
+        self.delta_t=0.1 #time step (seconds)
+        self.x_0=0.0 #Initial postion of ball in x-direction
+        self.y_0=0.0 #Initial postion of ball in y-direction
         
     def parse_argv(self):
         for arg in sys.argv[1:]:
@@ -29,14 +31,12 @@ class golfball:
         self.v_x_0=self.v_0*math.cos(math.radians(self.theta)) #Initial velocity in x-direction
         self.v_y_0=self.v_0*math.sin(math.radians(self.theta)) #Initial velocity in y-direction
 
-    def ideal(self): #Case 1: Ideal
-        #Parameters 
-        x=0.0 #Initial postion of ball in x-direction
-        y=0.0 #Initial postion of ball in y-direction
-        v_x=self.v_x_0 #Initial velocity in x-direction
-        v_y=self.v_y_0 #Initial velocity in y-direction
-        #Lists to store positions 
-        self.x_ideal=[]
+    def ideal(self): #Case 1: Ideal 
+        x=self.x_0 #Initializing starting boundaries
+        y=self.y_0 
+        v_x=self.v_x_0 
+        v_y=self.v_y_0 
+        self.x_ideal=[] #Lists to store positions 
         self.y_ideal=[]
         ideal_total_t=0 #Initializing time 
         while y>=0.0: #while the golf ball is in the air
@@ -48,19 +48,18 @@ class golfball:
             v_x=v_x 
             v_y=v_y-self.g*self.delta_t
             ideal_total_t+=self.delta_t #Time of flight 
-        vf_ideal=v_mag
+        vf_ideal=v_mag #Final velocity
         print(f'Ideal golf ball hit at {self.theta}: v_f (m/s)={vf_ideal} t_f (s)={ideal_total_t}')
  
-    def smooth_drag(self): #Case 2: Smooth golf ball with drag
-        #Parameters 
-        x=0.0 #Initial postion of ball in x-direction
-        y=0.0 #Initial postion of ball in y-direction
-        v_x=self.v_x_0 #Initial velocity in x-direction
-        v_y=self.v_y_0 #Initial velocity in y-direction
-        self.x_smooth_drag=[]
+    def smooth_drag(self): #Case 2: Smooth golf ball with drag  
+        x=self.x_0 #Initializing starting boundaries
+        y=self.y_0 
+        v_x=self.v_x_0 
+        v_y=self.v_y_0 
+        self.x_smooth_drag=[] #Lists to store positions
         self.y_smooth_drag=[]
-        C=0.5
-        smooth_drag_t=0
+        C=0.5 #Drag constant 
+        smooth_drag_t=0 #Initializing time 
         while y>=0.0:
             self.x_smooth_drag.append(x)
             self.y_smooth_drag.append(y)
@@ -70,62 +69,59 @@ class golfball:
             v_x=v_x-((C*self.rho*self.a*v_mag)/self.m)*v_x*self.delta_t
             v_y=v_y-(self.g*self.delta_t)-((C*self.rho*self.a*v_mag)/self.m)*v_y*self.delta_t
             smooth_drag_t+=self.delta_t
-        vf_smooth_drag=v_mag
+        vf_smooth_drag=v_mag #Final velocity 
         print(f'Smooth golf ball with drag hit at {self.theta}: v_f (m/s)={vf_smooth_drag} t_f (s)={smooth_drag_t}')
 
-    def dimpled_drag(self): #Case 3: Dimpled golf ball with drag
-        #Parameters 
-        x=0.0 #Initial postion of ball in x-direction
-        y=0.0 #Initial postion of ball in y-direction
-        v_x=self.v_x_0 #Initial velocity in x-direction
-        v_y=self.v_y_0 #Initial velocity in y-direction
-        self.x_dimple_drag=[]
+    def dimpled_drag(self): #Case 3: Dimpled golf ball with drag 
+        x=self.x_0 #Initializing starting boundaries 
+        y=self.y_0 
+        v_x=self.v_x_0 
+        v_y=self.v_y_0
+        self.x_dimple_drag=[] #Lists to store positions 
         self.y_dimple_drag=[]
         dimple_drag_t=0
-        while y>=0:
+        while y>=0: 
             self.x_dimple_drag.append(x)
             self.y_dimple_drag.append(y)
             x=x+v_x*self.delta_t
             y=y+v_y*self.delta_t
             v_mag=math.sqrt(v_x**2+v_y**2)
             if v_mag<=14:
-                C=0.5
+                C=0.5 #Drag constant 
             elif v_mag>14:
                 C=7/v_mag
             v_x=v_x-((C*self.rho*self.a*v_mag)/self.m)*v_x*self.delta_t
             v_y=v_y-(self.g*self.delta_t)-((C*self.rho*self.a*v_mag)/self.m)*v_y*self.delta_t
             dimple_drag_t+=self.delta_t
-        vf_dimple_drag=v_mag
+        vf_dimple_drag=v_mag #Final velocity 
         print(f'Dimpled golf ball with drag hit at {self.theta}: v_f (m/s)={vf_dimple_drag} t_f (s)={dimple_drag_t}')
 
-    def spin(self): #Case 4: Dimpled golf ball with drage and spin
-        #Parameters 
-        x=0.0 #Initial postion of ball in x-direction
-        y=0.0 #Initial postion of ball in y-direction
-        v_x=self.v_x_0 #Initial velocity in x-direction
-        v_y=self.v_y_0 #Initial velocity in y-direction
-        Sow=0.25 #s^-1
-        self.x_spin=[]
+    def spin(self): #Case 4: Dimpled golf ball with drage and spin  
+        x=self.x_0 #Initializing starting boundaries
+        y=self.y_0 
+        v_x=self.v_x_0 
+        v_y=self.v_y_0 
+        Sow=0.25 #s^-1 #Spin vector 
+        self.x_spin=[] #Lists to store positions
         self.y_spin=[]
-        spin_t=0
+        spin_t=0 #Initializing time 
         while y>=0:
             self.x_spin.append(x)
             self.y_spin.append(y)
             x=x+v_x*self.delta_t
             y=y+v_y*self.delta_t
             v_mag=math.sqrt(v_x**2+v_y**2)
-            if v_mag<=14:
+            if v_mag<=14: #Drag constant 
                 C=0.5
             elif v_mag>14:
                 C=7/v_mag
             v_x=v_x+(-((C*self.rho*self.a*v_mag)/self.m)-(Sow/self.m)*v_y)*self.delta_t
             v_y=v_y+(-((C*self.rho*self.a*v_mag)/self.m)-(Sow/self.m)*v_x-self.g)*self.delta_t
             spin_t+=self.delta_t
-        vf_spin=v_mag
+        vf_spin=v_mag #Final velocity 
         print(f'Dimpled golf ball with drag and spin hit at {self.theta}: v_f (m/s)={vf_spin} t_f (s)={spin_t}')
 
-    #Plotting trajectories
-    def plot(self): 
+    def plot(self): #Plotting trajectories
         plt.figure()
         plt.plot(self.x_ideal,self.y_ideal,label='Ideal')
         plt.plot(self.x_smooth_drag,self.y_smooth_drag,label='Smooth ball with drag')
