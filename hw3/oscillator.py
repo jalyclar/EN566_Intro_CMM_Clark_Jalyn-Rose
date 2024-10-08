7e01ab65c9e713640665028468e4d5b9684a16e9
jalyclar <jalynroseclark@gmail.com>
2024-10-08 19:09:27 -0400

    begining progress

--- hw3/oscillator.py
+++ hw3/oscillator.py
@@ -2,89 +2,105 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-def linear(DEBUG=False):
+def undamped(DEBUG=False): #Only using euler-cromer method 
+
+    #Variables 
     g=9.8 #m/s^2
     l=9.8 #m
-
-    #Linear motion nondamped 
     omega_values=[]
     theta_values=[]
     t_values=[]
-
     delta_t=0.01 #second
     t=0 #seconds 
     omega=1 #m/s^2 initial velocity 
     theta=math.radians(15) #rad initial angle 
-    for i in range(1000):
+
+    #Numerical caluclations 
+    for i in range(10000): #proving a signficant amount of steps 
         omega_values.append(omega)
         theta_values.append(theta)
         t_values.append(t)
-        omega=omega-(g/l)*theta*delta_t
+        omega=omega-(g/l)*theta*delta_t 
         theta=theta+omega*delta_t
         t=t+delta_t
-        if abs(omega) < 1e-5 and abs(theta) < 1e-5:
+        if abs(omega)<=1e-8 and abs(theta)<=1e-8: #exit loop when close to 0 
             break
     if DEBUG==True:
         print(f"omega: {omega_values}")
         print(f"angle: {theta_values}")
         print(f"time values: {t_values}")
 
-    plt.figure()
-    plt.plot(t_values, theta_values,label="Theta(t)")
-    plt.plot(t_values,omega_values,label="Omega(t)")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Angular velocity and Angular Discplacment")
-    plt.title("Angular velocity and Angular Displacment vs Time")
-    plt.legend()
+    #Plot
+    fig,(ax1,ax2)=plt.subplots(2,1, figsize=(8,6))
+    ax1.plot(t_values,theta_values,color='blue')
+    ax1.set_ylabel("Angular Discplacment")
+    ax2.plot(t_values,omega_values,color='pink')
+    ax2.set_ylabel("Angular velocity")
+    fig.supxlabel("Time (s)")
+    fig.suptitle("Undamped Solution")
+    for ax in [ax1,ax2]:
+        ax.axhline(y=0, color='black', linewidth=1, zorder=1) 
+        y_min,y_max = ax.get_ylim()
+        limit=max(abs(y_min),abs(y_max))
+        ax.set_ylim(-limit,limit)
+    plt.tight_layout()
     plt.show()
 
-def damped(DEBUG=False):
+def damped(DEBUG=False): #Only using euler-cromer method 
+   
+   #Variables 
     g=9.8 #m/s^2
     l=9.8 #m
     gamma=0.25 #s^-1
-    alpha_d=0.2 #rad/s^2
-
-    #Linear motion nondamped 
+    alpha_d=0.2 #rad/s^2 
     d_omega_values=[]
     d_theta_values=[]
     d_t_values=[]
-
     delta_t=0.01 #second
     t=0 #seconds 
     omega=1 #m/s^2 initial velocity 
     theta=math.radians(15) #rad initial angle
     q=2*gamma #Damp factor
 
-    for i in range(10000):
-        if q<(2*math.sqrt(g/l)): #underdamped
-            d_omega_values.append(omega)
-            d_theta_values.append(theta)
-            d_t_values.append(t)
+    #Condition
+    if q<(2*math.sqrt(g/l)): #Determing type of dampining based on damp factor 
+        type_damp="Underdamped"
+    elif q==(2*math.sqrt(g/l)):
+        type_damp="Critically Damped"
+    elif q>(2*math.sqrt(g/l)):
+        type_damp="Overdamped" 
 
-            omega=omega-((g/l)*theta*delta_t+q*omega*delta_t)
-            theta=theta+omega*delta_t
-            t=t+delta_t
-            if abs(omega) < 1e-5 and abs(theta) < 1e-5:
-                break
+    #Numerical calculation 
+    for i in range(10000): #Over a signficant amount of steps 
+        d_omega_values.append(omega)
+        d_theta_values.append(theta)
+        d_t_values.append(t)
+        omega=omega-((g/l)*theta*delta_t+q*omega*delta_t)
+        theta=theta+omega*delta_t
+        t=t+delta_t
+        if abs(omega)<=1e-8 and abs(theta)<=1e-8:
+            break
     if DEBUG==True:
         print(f"omega: {d_omega_values}")
         print(f"angle: {d_theta_values}")
         print(f"time values: {d_t_values}")
 
-        #elif q=3*omega: #critically damped
-        #elif q>2*omega: #overdamped 
-    
-    plt.figure()
-    plt.plot(d_t_values, d_theta_values,label="Theta(t)")
-    plt.plot(d_t_values,d_omega_values,label="Omega(t)")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Angular velocity and Angular Discplacment")
-    plt.title("Angular velocity and Angular Displacment vs Time - Damped")
-    plt.legend()
+    #Plot 
+    fig,(ax1,ax2)=plt.subplots(2,1, figsize=(8,6))
+    ax1.plot(d_t_values, d_theta_values,color='pink')
+    ax1.set_ylabel("Angular Discplacment")
+    ax2.plot(d_t_values,d_omega_values,color='blue')
+    ax2.set_ylabel("Angular velocity")
+    fig.supxlabel("Time (s)")
+    fig.suptitle(f"{type_damp} Solution")
+    for ax in [ax1,ax2]:
+        ax.axhline(y=0, color='black', linewidth=1, zorder=1) 
+        y_min,y_max = ax.get_ylim()
+        limit=max(abs(y_min),abs(y_max))
+        ax.set_ylim(-limit,limit)
+    plt.tight_layout()
     plt.show()
-    
-
 
 #calling functions 
-#linear(DEBUG=False)
+undamped(DEBUG=False)
 damped(DEBUG=False)
