47061714946cbb8adb43faba84431ce153d0d0e0
jalyclar <jalynroseclark@gmail.com>
2024-10-25 13:32:05 -0400

    still WIP

--- hw3/poisson.py
+++ hw3/poisson.py
@@ -4,8 +4,8 @@ from scipy.optimize import curve_fit
 
 #In 2d
 
-################     Adjust all functions after Gauss, fix plots, and analysis      ##########################
-################     Fix centering of dipole     ##########################
+################     WHats wrong with jacobiiiiiiiiii !!!!!!!   ##########################
+################     when WHILE statment instead for for range iterations ???????    ##########################
 ################     Adjust structure of code     ##########################
 ################    Allow command line input      ##########################
 
@@ -101,7 +101,7 @@ def jacobi_relax(DEBUG=False,grid_size=10,tolerance=10**-3):
     x=np.linspace(-10,10,grid_size) #R boundaries 
     y=np.linspace(-10,10,grid_size)
     X,Y=np.meshgrid(x,y) #for contour plot 
-    V=np.zeros((grid_size,grid_size)) #Initializing grid 
+    #V=np.zeros((grid_size,grid_size)) #Initializing grid 
     new_V=np.zeros((grid_size,grid_size)) #For updated grid
     rho=np.zeros((grid_size,grid_size)) #For modified caluclation 
     center_grid=grid_size//2 #finding the center of the grid
@@ -119,19 +119,20 @@ def jacobi_relax(DEBUG=False,grid_size=10,tolerance=10**-3):
         print(f"q_pos_y: {q_pos_y}\n\n")
         print(f"q_neg_x: {q_neg_x}\n\n")  
         print(f"q_neg_y: {q_neg_y}\n\n")     
-    V[q_pos_x,q_pos_y]=Q
-    V[q_neg_x,q_neg_y]=-Q
+    new_V[q_pos_x,q_pos_y]=Q
+    new_V[q_neg_x,q_neg_y]=-Q
     rho[q_pos_x,q_pos_y]=Q
     rho[q_neg_x,q_neg_y]=-Q
     if DEBUG==True:
-        print(f"Initial V(r) grid: {V}\n\n")
+        print(f"Initial V(r) grid: {new_V}\n\n")
     #iterations=grid_size**2
     #if DEBUG==True:
         #print(f'Number of iterations for convergence: {iterations}\n\n')
-    converge=False
+    iterations=1000000
     N_iter=0
-    while not converge:
-        change=0
+    for iter in range(iterations):
+        old_V=new_V.copy()
+        del_V=0
         for i in range(1,grid_size-1):
             for j in range(1,grid_size-1):
                 if (i,j)==(q_pos_x,q_pos_y) or (i,j)==(q_neg_x,q_neg_y): #skipping past intial dipole point charges 
@@ -139,21 +140,23 @@ def jacobi_relax(DEBUG=False,grid_size=10,tolerance=10**-3):
                 r=np.sqrt((i-center_grid)**2+(j-center_grid)**2) #Checking to see where point is relative to center
                 if r<10: #boundary condition
                     if rho[i,j]==0: #using laplace where there is no cahrge density 
-                        V[i,j]=1/4*(V[i+1,j]+V[i-1,j]+V[i,j+1]+V[i,j-1])
+                        new_V[i,j]=1/4*(old_V[i+1,j]+old_V[i-1,j]+old_V[i,j+1]+old_V[i,j-1])
                     else: #using poisson where there is charge density 
-                        V[i,j]=1/4*(V[i+1,j]+V[i-1,j]+V[i,j+1]+V[i,j-1])+(rho[i,j]*h**2/e)
-                    del_V=np.absolute(new_V[i,j]-V[i,j]) #max chnage in V
-                    #if del_V<change:
-                    change+=del_V
+                        new_V[i,j]=1/4*(old_V[i+1,j]+old_V[i-1,j]+old_V[i,j+1]+old_V[i,j-1])+(rho[i,j]*h**2/e)
+                    #del_V=np.absolute(new_V[i,j]-V[i,j]) #max chnage in V
+                    #change=max(change,del_V)
                     #if DEBUG==True:
                         #print(f'Current delta(V): {change:.8f}\n\n')
-        new_V[:,:]=V[:,:]
+                    del_V+=np.absolute(new_V[i,j]-old_V[i,j])
+        N_iter=iter
+        #del_V=np.max(np.absolute(new_V[i,j]-old_V[i,j]))
         #print(f'{new_V}\n\n')
-        N_iter+=1
         if DEBUG==True:
             print(f'Current iteration: {N_iter}\n\n')
-        if change<tolerance:
-            converge=True 
+        if iter>5 and del_V<tolerance:
+            break
+    #print(f'Converged after {iter} iterations.\n\n')
+    #print(f"Final V(r) grid: {new_V}\n\n")
     return X,Y,new_V,N_iter
 
 def sor(DEBUG=False,grid_size=10,tolerance=10**-3):
@@ -199,8 +202,8 @@ def sor(DEBUG=False,grid_size=10,tolerance=10**-3):
         #print(f'Number of iterations for convergence: {iterations}\n\n')
     N_iter=0
     #for iter in range(iterations):
-    converge=False
-    while not converge:
+    iterations=1000000
+    for iter in range(iterations):
         change=0
         for i in range(1,grid_size-1):
             for j in range(1,grid_size-1):
@@ -221,10 +224,10 @@ def sor(DEBUG=False,grid_size=10,tolerance=10**-3):
                    #V[i,j]=V_new[i,j]
                     change=max(change,del_V)
                 V[i,j]=V_new[i,j]
-        N_iter+=1
+        N_iter=iter
         #print(f'Change and iter step: {change:.8f} and {N_iter}')
         if change<tolerance:
-            converge=True
+            break
     if DEBUG==True:
         print(f"Final V(r) grid: {V_new}\n\n")
     #print(f'Converged after {N_iter} iterations.\n\n')
@@ -236,7 +239,7 @@ def sor(DEBUG=False,grid_size=10,tolerance=10**-3):
     #plt.show()                        
     return X,Y,V,N_iter
 
-def partone(DEBUG=False):
+def partone(DEBUG=True):
     grid_size=10
     X,Y,new_V,N_iter=jacobi_relax(DEBUG=False,grid_size=grid_size,tolerance=10**-3)
     if DEBUG==True:
@@ -267,7 +270,7 @@ def partone(DEBUG=False):
     plt.show()
 
 def partwo(DEBUG=False):
-    error_limits=np.logspace(-9,-1,num=20)
+    error_limits=np.logspace(-11,-1,num=20)
     N_iter_values=[]
     errors=[]
     for tolerance in error_limits:
@@ -287,14 +290,14 @@ def partwo(DEBUG=False):
     plt.show()
 
 def partthree(DEBUG=True):
-    grids=np.linspace(10,100,dtype=int) #grids=np.linspace(10,100,dtype=int) #For final analysis 
+    grids=np.linspace(10,60,5,dtype=int) 
     iter_jacob=[]
     iter_sor=[]
     if DEBUG==True:
         print(f'grid sizes to use: {grids}')
-    for grid_size in grids:
-        _,_,_,N_iter=jacobi_relax(False,grid_size,10**-3)
-        _,_,_,N_iter_s=sor(False,grid_size,10**-3)
+    for L in grids:
+        _,_,_,N_iter=jacobi_relax(False,L,10**-3)
+        _,_,_,N_iter_s=sor(False,L,10**-3)
         iter_jacob.append(N_iter)
         iter_sor.append(N_iter_s)
     if DEBUG==True:
@@ -315,8 +318,8 @@ if __name__=="__main__":
     #jacobi_relax()
     #sor()
     #partone()
-    #partwo()
-    partthree()
+    partwo()
+    #partthree()
 
 
 
