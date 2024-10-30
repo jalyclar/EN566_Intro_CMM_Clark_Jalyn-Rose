9ca6c836ac23160d77d9fbda7c7928c289f1f914
jalyclar <jalynroseclark@gmail.com>
2024-10-30 17:45:37 -0400

    In class work

--- hw4/rnumbers.py
+++ hw4/rnumbers.py
@@ -4,17 +4,36 @@ import random
 import matplotlib.pyplot as plt 
 import numpy as np
 
-def partone(N=1000,bins=10):
-    rnum_values=[]
+def partone():
+    N_values=[1000,1000000]
+    bin_values=[10,20,50,100]
 
+    for N in N_values:
+        rnum_values=[]
+        for i in range(N):
+            rnum=random.random()
+            rnum_values.append(rnum)
+        
+        for bin_size in bin_values:
+            plt.hist(rnum_values,bin_size)
+            plt.xlabel('Random Number')
+            plt.ylabel('Frequency')
+            plt.title(f'Random Numbers - Bin Size={bin_size}')
+            plt.show()
+
+def parttwo():
+    sigma=1.0
+    N=1000
+    p_values=[]
     for i in range(N):
+        #Use Rejection method  
+        #Use LGC to generate linear combination of random numbers 
         rnum=random.random()
-        rnum_values.append(rnum)
+        p=(1/sigma*np.sqrt(2*np.pi))**(-(x**2)/2*sigma**2)
+        p_values.append(p)
+    
 
-    plt.hist(rnum_values,bins)
-    plt.xlabel('Random Number')
-    plt.ylabel('Frequency')
-    plt.show()
+
 
 if __name__=="__main__":
     partone()
